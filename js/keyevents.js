b398bd0f66a693ea2d6274d58797f0aaa520049f
Yuto Ichinose <yuto.0526.sano@gmail.com>
2021-12-22 22:03:43 +0900

    キーダウン時の処理

---
+++ js/keyevents.js
@@ -0,0 +1,42 @@
+/**
+* キーが押されたとき
+* @param {*} ev キーボード 
+*/
+function typekeydown(ev) {
+    if (ev.key=="ArrowLeft"||ev.key=="ArrowRight") {
+        keycheckX=ev.key;
+    } else if(ev.key=="ArrowUp"||ev.key=="ArrowDown"){
+        keycheckY=ev.key;
+    }
+}
+/**
+* キーが離されたとき
+* @param {*} ev キーボード 
+*/
+function typekeyup(ev) {
+    keycheckX="";
+    keycheckY="";
+}
+/**
+* キーボードでプレイヤーを動かす処理
+* @param {*} ev 押されてるキー
+*/
+function keymove(ev) {
+    switch (ev) {
+        case "ArrowLeft":
+            Playermove(document.getElementById("left_btn"));
+            break;
+        case "ArrowRight":
+            Playermove(document.getElementById("right_btn"));
+            break;
+        case "ArrowUp":
+            Playermove(document.getElementById("up_btn"));
+            break;
+        case "ArrowDown":
+            Playermove(document.getElementById("down_btn"));
+            break;
+
+        default:
+            break;
+    }
+}
